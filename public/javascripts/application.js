687508b191238c2d4eff49be0e5b8cab966ec0bb
Kellective <kellective@gmail.com>
2010-11-24 15:58:10 +1300

    ui stuff, slider

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+	
+	var step = $( "#slider" ).slider( "option", "step" );
+	var max = $( "#slider" ).slider( "option", "max" );
+	var max = $( "#slider" ).slider( "option", "min" );
+	
+    $("#slider").slider({max:7, min:1, step:1 });
+
+	
+
+  });
